0ffaaf3aaa3fdd967dbaa129894e2f4333eeb5a5
Natalia Layson <nlayson@berkeley.edu>
2015-09-16 12:12:01 -0700

    hi

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,16 @@
 class Foobar
-  # Q4 CODE HERE
+  def self.baz(int_array)
+  	int_array = int_array.map(&:to_i)
+  	new_array = []
+  	(0..int_array.length-1).each do |i|
+  		a = int_array[i]
+  		if a.to_i.even?
+  			new_array << a+2
+  		end
+  	end
+  	new_array = new_array.uniq
+  	return new_array.inject{|sum,x| sum + x}
+  end
 end
+
+
