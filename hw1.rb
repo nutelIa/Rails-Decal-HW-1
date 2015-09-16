4eaff329d5d9ed4ec4948614a16766445b48e367
Natalia Layson <nlayson@berkeley.edu>
2015-09-16 11:25:57 -0700

    yay

--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,35 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+	num = a+b
+	return num*num
 end
 
-def sort_array_plus_one(a)
-  # Q2 CODE HERE
+def sort_array_plus_one(ls)
+	ls2=[]
+	ls3=[]
+
+	(0..ls.length-1).each do |j|
+	    ls2.push(ls[j]+1)
+	end
+	temp = ls2.pop
+	ls3 << temp
+	(0..ls2.length-1).each do |i|
+	    temp = ls2[i]
+	    temp2 = ls3.length
+	    (0..ls3.length-1).each do |k|
+	        if temp < ls3[k]
+	            ls3.insert(k,temp)
+	            break
+	        end
+	        if temp > ls3[temp2-1]
+	            ls3 << temp
+	            break
+	        end
+	    end
+	end
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
